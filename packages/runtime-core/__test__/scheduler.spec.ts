8f4630a4da541ac11944f97d7f0ca81696d1e76b
phil <phil_litian@163.com>
2024-11-01 21:07:24 +0800

    feat: add gitignore

--- packages/runtime-core/__test__/scheduler.spec.ts
+++ packages/runtime-core/__test__/scheduler.spec.ts
@@ -1,5 +1,22 @@
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { nextTick } from '../src/scheduler'
 
 describe('scheduler', () => {
-  it('nextTick', () => {})
+  it('nextTick', async () => {
+    const calls: string[] = []
+    const dummyThen = Promise.resolve().then()
+    const job1 = () => {
+      calls.push('job1')
+    }
+    const job2 = () => {
+      calls.push('job2')
+    }
+
+    nextTick(job1)
+    job2()
+    expect(calls.length).toBe(1)
+    await dummyThen
+    expect(calls.length).toBe(2)
+    expect(calls).toEqual(['job2', 'job1'])
+  })
 })
